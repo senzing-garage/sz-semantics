ccfab62e9f61ec97a4c3ad69f5fc66a70b5774a4
Michael Dockter <michael@dockter.com>
2026-02-16 16:22:52 -0500

    #24 Fix linting errors (#34)
    * #24 Fix linting errors
    
    * #24 Fix linting issues
    
    * #24 Fix linting issues
    
    * #24 Fix linting issues
    
    * #24 Fix linting issues
    
    * #24 Fix linting issues
    
    * #24 Fix linting issues

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -10,6 +10,7 @@ see copyright/license https://github.com/senzing-garage/sz-semantics/README.md
 """
 
 import json
+import typing
 
 from sz_semantics import Mask
 
@@ -22,7 +23,7 @@ def test_mask(
     Verify that the mask/unmask roundtrip protects and preserves the
     PII values correctly.
     """
-    exp_data: dict = {
+    exp_data: dict[str, typing.Any] = {
         "RESOLVED_ENTITY": {
             "ENTITY_ID": 1,
             "ENTITY_NAME": "Robert Smith",
@@ -32,10 +33,10 @@ def test_mask(
     }
 
     exp_text: str = (
-        """
-        {"RESOLVED_ENTITY": {"ADDRESS_DATA": ["HOME: HOME_1"], "ENTITY_ID": 1, "ENTITY_NAME": "ENTITY_NAME_1", "IDENTIFIER_DATA": ["EMAIL: EMAIL_1"]}}
-    """.strip()
-    )  # pylint: disable=C0301
+        '{"RESOLVED_ENTITY": {"ADDRESS_DATA": ["HOME: HOME_1"],'
+        ' "ENTITY_ID": 1, "ENTITY_NAME": "ENTITY_NAME_1",'
+        ' "IDENTIFIER_DATA": ["EMAIL: EMAIL_1"]}}'
+    )
 
     sz_mask: Mask = Mask()
     masked_data: dict = sz_mask.mask_data(exp_data)  # type: ignore
@@ -43,7 +44,7 @@ def test_mask(
     obs_text: str = json.dumps(masked_data, sort_keys=True)
     assert exp_text == obs_text
 
-    obs_data: dict = json.loads(sz_mask.unmask_text(obs_text))
+    obs_data: dict[str, typing.Any] = json.loads(sz_mask.unmask_text(obs_text))
     assert sorted(exp_data.items()) == sorted(obs_data.items())
 
 
